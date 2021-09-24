8a939372661d666818a1f4c6a49b84ce8852ecd9
Ada-Codina <89468347+Ada-Codina@users.noreply.github.com>
2021-09-24 09:34:15 -0600

    Update pii_redactor.sh

--- pii_redactor.sh
+++ pii_redactor.sh
@@ -1,14 +1,14 @@
 #!/bin/bash  
 
 yml="cluster.yml"
-REDACT="******"
+REDACT="****************"
 
-sed -e " s/\(address: \)\(.*\)/\1$REDACT/;
+sed -e " /#.*/ d;
+s/\(address: \)\(.*\)/\1$REDACT/;
 s/\(user: \)\(.*\)/\1$REDACT/;
 s/\(ssh_key_path: \)\(.*\)/\1$REDACT/;
-s/\(ssh_key: \)\(.*\)/\1$REDACT/;
 s/\(ssh_cert_path: \)\(.*\)/\1$REDACT/;
-s/\(ssh_cert: \)\(.*\)/\1$REDACT/;
 s/\(hostname_override: \)\(.*\)/\1$REDACT/;
 s/\(internal_address: \)\(.*\)/\1$REDACT/;
-s/\(key: \)\(.*\)/\1$REDACT/" $yml > redacted_clus.yml
+s/\(key: \)\(.*\)/\1$REDACT/;
+/-----BEGIN/,/-----END/{/-----BEGIN/n;/-----END/!{s/./        $REDACT/}};" $yml > redacted_clus.yml
