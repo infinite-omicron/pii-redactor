674fc99604bac109f9cc2a71b478c1ced10c9ca9
Ada-Codina <89468347+Ada-Codina@users.noreply.github.com>
2021-11-16 19:04:50 -0600

    Implement command line arguments (#7)
    Co-authored-by: Ada-Codina <adacodina@gmail.com>
    Co-authored-by: Lucas Ramage <lucas.ramage@infinite-omicron.com>

--- pii_redactor.sh
+++ pii_redactor.sh
@@ -1,14 +1,65 @@
-#!/bin/bash  
+#!/bin/bash
+set -eu
 
-yml="cluster.yml"
-REDACT="****************"
+# variables in use
+version="0.0.1"
+REDACT="**********"
+debug=0
+verbose=0
+dir=$@
 
-sed -e " /#.*/ d;
-s/\(address: \)\(.*\)/\1$REDACT/;
+if [ -d "$dir" ]; then
+yml=$(find "$dir" -name "cluster.yml")
+# official version will write to actual .yml
+sed -i "s/\(address: \)\(.*\)/\1$REDACT/;
 s/\(user: \)\(.*\)/\1$REDACT/;
 s/\(ssh_key_path: \)\(.*\)/\1$REDACT/;
 s/\(ssh_cert_path: \)\(.*\)/\1$REDACT/;
 s/\(hostname_override: \)\(.*\)/\1$REDACT/;
 s/\(internal_address: \)\(.*\)/\1$REDACT/;
-s/\(key: \)\(.*\)/\1$REDACT/;
-/-----BEGIN/,/-----END/{/-----BEGIN/n;/-----END/!{s/./        $REDACT/}};" $yml > redacted_clus.yml
+1,/ key:.*/{s// key: $REDACT/};
+/-----BEGIN/,/-----END/{/-----BEGIN/n;/-----END/!{s/./        $REDACT/}};" "$yml"
+echo "Success"
+else
+echo "Could not find file."
+fi
+
+usage() {
+cat <<EOF
+USAGE: 
+pii_redactor [command] || <directory that holds cluster.yml>
+
+GLOBAL COMMANDS:
+  -h, --help            Show this message
+  -d, --debug           Output debug messages
+  -v, --verbose         verbose mode
+  -ver, --version       Print version
+EOF
+}
+
+while [ ${#} -gt 0 ]
+do
+  a=${1}
+  shift
+  case ${a} in 
+    -h|--help) 
+      usage
+      ;;
+    -d|--debug)
+      debug=1
+      echo "Debug is on"
+     ;;   
+    -v|--verbose)
+      verbose=1
+      echo "Verbose is on"
+      ;;
+    -ver|--version)
+      echo "pii_redactor.sh version is ${version}"
+      ;;
+    "$dir")
+      ;; 
+    *) 
+     echo "Invalid Input. Try --help or -h"
+     ;;
+  esac
+done
